2e8412c75bd18a2e4160d972b21ff1390a294547
AnaMaria2662 <115395707+AnaMaria2662@users.noreply.github.com>
2022-11-25 17:37:18 +0200

    Update main.cpp

--- Graficul functiilor-IP/main.cpp
+++ Graficul functiilor-IP/main.cpp
@@ -57,6 +57,7 @@ int main()
     if(coordx>=339&&coordx<=440&&coordy>=330&&coordy<=370)
         {
             initwindow(800,800);
+            readimagefile("image1_1.jpg",0,0,800,800);
             ....//de facut urmatoarea fereastra
             getch();
             closegraph();
@@ -64,6 +65,7 @@ int main()
         else if(coordx>=330&&coordx<=460&&coordy>=430&&coordy<=470)
             {
                 initwindow(800,800);
+                readimagefile("image1_1.jpg",0,0,800,800);
                 settextstyle(7, HORIZ_DIR, 5);
                 outtextxy(100,200,"Proiectul a fost realizat de:");
                 settextstyle(7, HORIZ_DIR, 4);
