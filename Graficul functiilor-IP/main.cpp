af3cd1a6cd69faa77d8ae263cd2ed07c1304c0b2
AnaMaria2662 <115395707+AnaMaria2662@users.noreply.github.com>
2022-11-28 01:29:20 +0200

    Update main.cpp

--- Graficul functiilor-IP/main.cpp
+++ Graficul functiilor-IP/main.cpp
@@ -16,11 +16,17 @@ Evaluator:
 -stive?
 */
 #include<iostream>
-#include<windows.h>
-#include<mmsystem.h>
+//#include<windows.h>
+//#include<mmsystem.h>
 #include <graphics.h>
 #include <winbgim.h>
+#define MIN 100000001
+#define STANGA width/2-500
+#define DREAPTA width/2+500
+#define TOP height/5+100
+#define BOTTOM height/2+200
 using namespace std;
+//click Ana-Maria
 void click(int &coordxclick, int &coordyclick )
 {
     int x, y;
@@ -30,25 +36,70 @@ void click(int &coordxclick, int &coordyclick )
     coordxclick=x;
     coordyclick=y;
 }
+//functia sinus neterminata Ana-Maria
+int sinus(int x)
+{
+    if(x!=0)
+        return 1;//functia noastra
+    else return 0;
+}
+//aflare min si max Ana-Maria
+void aflareminsimax()
+{
+    int i,x,y,punctmin,punctmax;
+    for(i=0;i<=(DREAPTA-STANGA);i++)
+    {
+        x=A+i*(B-A)/DREAPTA-STANGA);
+        y=f(x);
+        if((-1)*MIN>y)punctmax=(-1)*MIN;
+                else punctmax=y;
+        if(MIN>y)punctmin=MIN;
+                else punctmin=y;
+    }
+}
+//trasare graficul functiei introduse Ana-Maria
+void graficfunctie()
+{
+    int x,y,xecran,yecran,xpunctactual,ypunctactual,i;
+    x=A;
+    y=f(x);
+    aflareminsimax();
+    xecran=(DREAPTA-STANGA)*x/(B-A))+(B*STANGA-A*Dreapta)/(B-A);
+    yecran=(BOTTOM-TOP)*y/(punctmax-punctmin))+(PUNCTMAX*TOP-punctmin*BOTTOM)/(punctmax-punctmin);
+    for(i=0;i<=DREAPTA-STANGA;i++)
+    {
+        x=A+i*(B-A)/(DREAPTA-STANGA);
+        xpunctactual=(int)((DREAPTA-STANGA)*x/(B-A)+(B*STANGA-A*DREAPTA)/(B-A));
+        ypunctactual=(int)((BOTTOM-TOP)*y/(punctmax-punctmin)+(punctmax*TOP-punctmin*BOTTOM)/(punctmax-punctmin));
+        line(xecran,yecran,xpunctactual,ypunctctual);
+        xecran=xpunctactual;
+        yecran=ypunctactual;
+    }
+}
 int main()
 {
-    int coordx, coordy, height, width;
+    int A,B,coordx, coordy, height, width;
+    //interval Ana-Maria
+    cout<<"Introduceti capetele intervalului in care doriti sa observati graficul functiei:";
+    cin>>A>>B;
+    cout<<"Intervalul este:"<<"["<<A<<","<<B<<"]";
+    delay(500);
 
-    //window initialization
+    //window initialization Emilia
     height=GetSystemMetrics(SM_CYSCREEN);
     width=GetSystemMetrics(SM_CXSCREEN);
     initwindow(width,height);
 
-    //sunet
+    //sunet Ana-Maria
     //PlaySound(TEXT("sound2.wav"),NULL,SND_ASYNC);
 
-    //background image
+    //background image Ana-Maria
     readimagefile("image1_1.jpg",0,0,width,height);
 
-    //imagine pt limba romana
+    //imagine pt limba romana Ana-Maria
     readimagefile("romana.jpg",width-80,(height/5-140),width-30,(height/5-115));
 
-    //text
+    //text Ana-Maria+Emilia
     setcolor(WHITE);
     settextstyle(8, HORIZ_DIR, 8 );
     settextjustify(CENTER_TEXT,CENTER_TEXT);
@@ -63,11 +114,11 @@ int main()
     outtextxy(width/2,height/2+150,"Contact");
     rectangle(width/2-119,height/2+110,width/2+117,height/2+165);
 
-    //buttons
+    //buttons Ana-Maria
     click(coordx,coordy);
     if(coordx>=(width/2-100)&&coordx<=(width/2+98)&&
                     coordy>=(height/2-40)&&coordy<=(height/2+12))
-        {
+        {//Emilia
             initwindow(width,height);
             readimagefile("image1_1.jpg",0,0,width,height);
             settextstyle(8, HORIZ_DIR, 6 );
@@ -81,6 +132,7 @@ int main()
             getch();
             closegraph();
         }
+        //Ana-Maria
         else if(coordx>=(width/2-119)&&coordx<=(width/2+117)&&
                     coordy>=(height/2+110)&&coordy<=(height/2+165))
             {
@@ -97,7 +149,7 @@ int main()
                 getch();
                 closegraph();
             }
-                //pentru limba engleza
+                //pentru limba engleza Ana-Maria
                 else if(coordx>=(width-80)&&coordx<=(width-30)&&
                             coordy>=(height/5-140)&&coordy<=(height/5-115))
                         readimagefile("engleza.jpg",width-80,(height/5-140),width-30,(height/5-115));
