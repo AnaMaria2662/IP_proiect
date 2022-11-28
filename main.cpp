5c3a76c6b5b65c102afa4f5f0df4bd76cee525bc
AnaMaria2662 <115395707+AnaMaria2662@users.noreply.github.com>
2022-11-28 23:27:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,17 +19,11 @@ Evaluator:
 */
 #include<iostream>
 #include<windows.h>
-#include<mmsystem.h>
 #include<graphics.h>
 #include<winbgim.h>
 #include<cstring>
 #define MIN 100000001
-#define STANGA 500
-#define DREAPTA 1000
-#define TOP 0
-#define BOTTOM 0
 using namespace std;
-//click Ana-Maria
 void click(int &coordxclick, int &coordyclick )
 {
     int x, y;
@@ -39,15 +33,13 @@ void click(int &coordxclick, int &coordyclick )
     coordxclick=x;
     coordyclick=y;
 }
-//functia  neterminata Ana-Maria
 int f(int x)
 {
     if(x!=0)
-        return  0;//functia noastra
+        return  0;
     else return 0;
 }
-//aflare min si max Ana-Maria
-void aflareminsimax(int A, int B)
+void aflareminsimax(int A, int B, int DREAPTA,int STANGA)
 {
     int i,x,y,punctmin,punctmax;
     for(i=0;i<=(DREAPTA-STANGA);i++)
@@ -61,13 +53,12 @@ void aflareminsimax(int A, int B)
     }
 }
 
-//trasare graficul functiei introduse Ana-Maria
-void graficfunctie(int A, int B,int punctmax, int punctmin)
+void graficfunctie(int A,int B,int punctmax,int punctmin,int DREAPTA,int STANGA,int BOTTOM,int TOP)
 {
     int x,y,xecran,yecran,xpunctactual,ypunctactual,i;
     x=A;
     y=f(x);
-    aflareminsimax(A,B);
+    aflareminsimax(A,B,DREAPTA,STANGA);
     xecran=((DREAPTA-STANGA)*x/(B-A))+(B*STANGA-A*DREAPTA)/(B-A);
     yecran=((BOTTOM-TOP)*y/(punctmax-punctmin))+(punctmax*TOP-punctmin*BOTTOM)/(punctmax-punctmin);
     for(i=0;i<=DREAPTA-STANGA;i++)
@@ -83,65 +74,81 @@ void graficfunctie(int A, int B,int punctmax, int punctmin)
 
 int main()
 {
+    int STANGA,DREAPTA,TOP,BOTTOM;
     int coordx, coordy;
-    //Ana-Maria
     char s[256];
     int i,A,B;
+    cin.getline(s,256);
     if(strstr(s,"sin"))
-            for(i=0;i<=strlen(s)-1;i++)//citire functii
+            for(i=0;i<=strlen(s)-1;i++)
                 {
-                    i++;
+                    return;
                 }
-    cin>>A>>B;
+        else if(strstr(s,"cos"))
+        {
+            for(i=0;i<=strlen(s)-1;i++)
+                {
+                    return;
+                }
+        }
+        else if(strstr(s,"tg"))
+        {
+            for(i=0;i<=strlen(s)-1;i++)
+                {
+                    return;
+                }
+        }
+        else if(strstr(s,"ctg"))
+        {
+            for(i=0;i<=strlen(s)-1;i++)
+                {
+                    return;
+                }
+        }
+        else if(strstr(s,"log"))
+        {
+            for(i=0;i<=strlen(s)-1;i++)
+                {
+                    return;
+                }
+        }
 
-    //window initialization Emilia
+    cin>>A>>B;
     int height, width;
     height=GetSystemMetrics(SM_CYSCREEN);
     width=GetSystemMetrics(SM_CXSCREEN);
     initwindow(width,height);
-
-    //sunet Ana-Maria
+    STANGA=0;
+    DREAPTA=width;
+    TOP=0;
+    BOTTOM=height;
     PlaySound("sound2.wav",NULL,SND_ASYNC);
-
-    //background image Ana-Maria
     readimagefile("image1_1.jpg",0,0,width,height);
-
-    //imagine pt limba romana Ana-Maria
     readimagefile("romana.jpg",width-80,(height/5-140),width-30,(height/5-115));
-
-    //text Ana-Maria+Emilia
     setcolor(WHITE);
     settextstyle(8, HORIZ_DIR, 8 );
     settextjustify(CENTER_TEXT,CENTER_TEXT);
     outtextxy(width/2,height/4,"Graficul functiei");
     rectangle(width/2-428,height/2-253,width/2+425,height/2-172);
-
     settextstyle(8, HORIZ_DIR, 6);
     outtextxy(width/2,height/2,"Grafic");
     rectangle(width/2-100,height/2-40,width/2+98,height/2+12);
-
     settextstyle(8, HORIZ_DIR, 6);
     outtextxy(width/2,height/2+150,"Contact");
     rectangle(width/2-119,height/2+110,width/2+117,height/2+165);
-
-    //Emilia
     settextstyle(8, HORIZ_DIR, 3 );
     outtextxy(width/16,height/8,"Exit");
-
-    //buttons Ana-Maria
     click(coordx,coordy);
-    if(coordx>=(width/2-100)&&coordx<=(width/2+98)&&
-                 coordy>=(height/2-40)&&coordy<=(height/2+12))
-        {   //exit
+    if(coordx>=(width/2-100)&&coordx<=(width/2+98)&&coordy>=(height/2-40)&&coordy<=(height/2+12))
+        {
             setcolor(YELLOW);
             rectangle(width/2-100, height/2-40, width/2+98,height/2+12);
-            //Emilia
             initwindow(width,height);
             readimagefile("image1_1.jpg",0,0,width,height);
-            settextstyle(8, HORIZ_DIR, 6 );
+            settextstyle(8, HORIZ_DIR, 4 );
             settextjustify(CENTER_TEXT,CENTER_TEXT);
             outtextxy(width/4,height/4-10,"Introduceti functia aici:");
-            rectangle(width/4-379,height/3-10,width/4+376,height/4+15);
+            rectangle(width/3-379,height/3-10,width/4+376,height/4+15);
             settextstyle(8, HORIZ_DIR, 5 );
             settextjustify(CENTER_TEXT,CENTER_TEXT);
             outtextxy(width/2,height/2-20,"Evaluator functie");
@@ -149,31 +156,24 @@ int main()
             setcolor(WHITE);
             settextstyle(8, HORIZ_DIR, 3 );
             outtextxy(width-141,height/16-5,"Exit");
-            //Ana-Maria
             clearmouseclick(WM_LBUTTONUP);
             click(coordx,coordy);
-            if(coordx>=(width/2-379)&&coordx<=(width/2+376)&&
-                 coordy>=(height/2-10)&&coordy<=(height/2+15))
+            if(coordx>=(width/2-379)&&coordx<=(width/2+376)&&coordy>=(height/2-10)&&coordy<=(height/2+15))
                  {
                     setcolor(YELLOW);
                     rectangle(width/2-379,height/2-10,width/2+376,height/2+15);
-                    //aici trebuie sa facem caseta de text
                  }
-                 else if(coordx>=(width/2-379)&&coordx<=(width/2+376)&&
-                                coordy>=(height/2+20)&&coordy<=(height/2+250))
+                 else if(coordx>=(width/2-379)&&coordx<=(width/2+376)&&coordy>=(height/2+20)&&coordy<=(height/2+250))
                         {
                             setcolor(YELLOW);
                             rectangle(width/2-379,height/2+20,width/2+376,height/2+250);
-                            //aici trebuie sa scriem ce tine de evaluator
                         }
                         else if(coordx>=(width/16)&&coordx<=(width/10)&&coordy>=(height/5-140)&&coordy<=(height/5-115))
                                     exit(1);
             getch();
             closegraph();
         }
-        //Ana-Maria
-        else if(coordx>=(width/2-119)&&coordx<=(width/2+117)&&
-                    coordy>=(height/2+110)&&coordy<=(height/2+165))
+        else if(coordx>=(width/2-119)&&coordx<=(width/2+117)&&coordy>=(height/2+110)&&coordy<=(height/2+165))
             {
                 setcolor(YELLOW);
                 rectangle(width/2-119, height/2+110, width/2+117,height/2+165);
@@ -188,16 +188,14 @@ int main()
                 settextstyle(8, HORIZ_DIR, 4);
                 outtextxy(width/2-160,height/2+80,"Galatanu Emilia");
                 settextstyle(8, HORIZ_DIR, 3 );
-                outtextxy(width-1410,height/16-5,"Exit");
+                outtextxy(width-1310,height/16-5,"Exit");
                 click(coordx,coordy);
                 if(coordx>=(width/16)&&coordx<=(width/10)&&coordy>=(height/5-140)&&coordy<=(height/5-115))
                         exit(1);
                 getch();
                 closegraph();
             }
-                //pentru limba engleza Ana-Maria
-                else if(coordx>=(width-80)&&coordx<=(width-30)&&
-                            coordy>=(height/5-140)&&coordy<=(height/5-115))
+                else if(coordx>=(width-80)&&coordx<=(width-30)&&coordy>=(height/5-140)&&coordy<=(height/5-115))
                         readimagefile("engleza.jpg",width-80,(height/5-140),width-30,(height/5-115));
                         else if(coordx>=(width/16)&&coordx<=(width/10)&&coordy>=(height/5-140)&&coordy<=(height/5-115))
                                 exit(1);
